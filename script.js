f4319b4157bedef00e580981ef8825d067c423b8
RabdiyaProductions <kiranrabdiya@gmail.com>
2025-12-16 00:40:59 +0000

    Reset of repository

--- script.js
+++
@@ -1,9 +0,0 @@
-// Basic JS interactions
-
-console.log("FlowForm Vitality Loaded");
-
-document.querySelectorAll(".btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-            alert("Class Preview Coming Soon!");
-                });
-                });
